2f07290be817d9f026ebf04a2dc284ff47596fbf
Sarvika24 <63595437+Sarvika24@users.noreply.github.com>
2020-05-15 20:18:22 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# SHAPES-CASTLE
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,53 @@
+function setup() {
+  // creating canvas 
+  createCanvas(1300,700 );
+
+  // giving backround color 
+  background(1, 21, 62);
+
+  //filling rectangles
+  fill(164, 222, 0);
+  rect(0, 600, 1300, 100)
+
+  fill(246, 215, 236);
+  rect(400, 280, 320, 300)
+
+  fill(246, 215, 236)
+  rect(300, 150, 70, 430)
+
+  fill(246, 215, 236)
+  rect(750, 150, 70, 430)
+
+  fill(246, 215, 236)
+  rect(150, 110, 100, 470)
+
+  fill(246, 215, 236)
+  rect(870, 110, 100, 470)
+
+  fill (103, 216, 237)
+  rect(630 ,720 , 100,20)
+
+  //filling triangles
+
+  fill (103, 216, 237);
+  triangle(400, 260, 720, 260,560, 100);
+  
+  fill(103, 216, 237)
+  triangle(870,100,925,40,980,100);
+  
+  fill (103, 216, 237)
+  triangle(300,140,335,100,370,140);
+
+  
+
+  fill(103, 216, 237);
+  triangle(750,140,785,100,820,140);
+
+  
+
+  fill (103, 216, 237);
+  triangle(140, 100, 195, 40,250, 100);
+
+
+
+}
